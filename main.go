1bf66e52dc8ab7d79d6c207ee7def8c140932e2f
ikkkkeee <2212479414@qq.com>
2025-05-07 21:02:53 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello  world",
+		})
+	})
+	r.Run(":9090")
+}
